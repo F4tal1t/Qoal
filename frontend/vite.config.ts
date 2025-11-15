f4216375bf27e5805ef8dd9eb8da3c5bcf167fcc
Dibyendu <dibyendusahoo03@gmail.com>
2025-11-15 23:06:38 +0530

    routing fix hopefully

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  publicDir: 'public',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
